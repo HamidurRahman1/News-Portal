ef0a8524fef097b4b67d876c85ca4e34c8d61553
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-23 01:20:16 -0400

    adding a single user via properties file

--- src/main/java/com/hamidur/ss/config/SecurityConfig.java
+++ src/main/java/com/hamidur/ss/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hamidur.ss;
+package com.hamidur.ss.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
