dfa6cb04680c3aa3c69bde8d6105c95039f23a81
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-23 00:50:04 -0400

    adding security to project and a config

---
+++ src/main/java/com/hamidur/ss/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.hamidur.ss;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+}
