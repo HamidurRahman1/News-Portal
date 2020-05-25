01f63f16d45d3d378701c6a5b5237852a84c06f4
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-25 03:04:38 -0400

    adding context path to url

--- src/main/java/com/hamidur/ss/config/SecurityConfig.java
+++ src/main/java/com/hamidur/ss/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hamidur.ss.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,15 +10,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+    @Value("${server.servlet.context-path}")
+    private String appPath;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
         http
                 .authorizeRequests()
-                    .antMatchers("/app", "/app/api/public/**")
+                    .antMatchers(appPath, appPath+"/api/public/**")
                     .permitAll()
                     .and()
                 .formLogin()
+                    .permitAll()
+                    .and()
+                .logout()
                     .permitAll()
                     .and()
                 .httpBasic();
