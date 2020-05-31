477861f0ba5b2d66ff0463492fa16472c676c85b
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-31 18:37:50 -0400

    Hibernate annotations have been added to this Entity

--- src/main/java/com/hamidur/ss/dao/Author.java
+++ src/main/java/com/hamidur/ss/dao/Author.java
@@ -1,12 +1,38 @@
 package com.hamidur.ss.dao;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
+@Entity
+@Table(name = "authors")
 public class Author
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
     private Integer authorId;
+
+    @Column(name = "author_first_name")
     private String firstName;
+
+    @Column(name = "author_last_name")
     private String lastName;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "authors_articles",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "article_id")
+    )
     private Set<Article> articles;
 
     public Author() {}
@@ -49,6 +75,17 @@ public class Author
     public void setArticles(Set<Article> articles) {
         this.articles = articles;
     }
+
+    public void addArticle(Article article) {
+        articles.add(article);
+        article.getAuthors().add(this);
+    }
+
+    public void removeArticle(Article article) {
+        articles.remove(article);
+        article.getAuthors().remove(this);
+    }
+
 
     @Override
     public String toString() {
