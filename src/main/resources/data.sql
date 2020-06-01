8f51b0a9cab0ac32d98169ac032698f5cf60ee07
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-31 20:20:18 -0400

    data.sql added with few demo data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+insert into authors (author_first_name, author_last_name) values
+('a1_first_name', 'a1_last_name'),
+('a2_first_name', 'a2_last_name');
+
+insert into articles (article_title, article_body, article_publish_date) values
+('ar1_title', 'ar1_body', '2020-05-20'),
+('ar2_title', 'ar2_body', '2020-05-20');
