2b15eaa0472131cce224c80f290b11c181114614
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-31 20:02:43 -0400

    Hibernate annotations have been added to this Entity

--- src/main/java/com/hamidur/ss/dao/Comment.java
+++ src/main/java/com/hamidur/ss/dao/Comment.java
@@ -1,11 +1,28 @@
 package com.hamidur.ss.dao;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Objects;
 
+@Entity
+@Table(name = "comments")
 public class Comment
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Integer commentId;
+
+    @Column(name = "comment")
     private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private Article article;
 
     public Comment() {}
