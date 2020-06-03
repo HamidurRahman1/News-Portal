3802ce2eacbde28873905a004fd78cd5eb4512b4
Hamidur Rahman <Hamidur0405@gmail.com>
2020-06-03 01:31:49 -0400

    Adding /access-denied to SecurityConfig

--- src/main/java/com/hamidur/ss/rest/RestrictedRESTController.java
+++ src/main/java/com/hamidur/ss/rest/RestrictedRESTController.java
@@ -10,13 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-@RequestMapping("/api/v1/restricted")
+@RequestMapping("/api/v1/r")
 public class RestrictedRESTController
 {
     private final AuthorRepository authorRepository;
@@ -38,5 +43,26 @@ public class RestrictedRESTController
         Comment comment = commentRepository.findByCommentId(commentId);
         comment.setArticle(null);
         return new ResponseEntity<>(comment, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/access-denied", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> accessDenied(HttpServletRequest request, HttpServletResponse response)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null)
+        {
+            stringBuilder
+                    .append("User: ").append(authentication.getName())
+                    .append(" has tried to access protected resources without proper credentials.")
+                    .append("\n")
+                    .append("ResponseStatus: ").append(HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(stringBuilder.toString(), HttpStatus.FORBIDDEN);
+        }
+        stringBuilder
+                .append("Request URI is protected, requires proper credentials for access.")
+                .append("\n")
+                .append("ResponseStatus: ").append(HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(stringBuilder.toString(), HttpStatus.FORBIDDEN);
     }
 }
