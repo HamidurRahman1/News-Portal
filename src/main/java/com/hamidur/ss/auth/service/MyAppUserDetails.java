1ab86472dc1185e7084e4ffa1ca7e241e5d49a57
Hamidur Rahman <Hamidur0405@gmail.com>
2020-06-03 00:29:21 -0400

    UserDetails have been implemented and added

---
+++ src/main/java/com/hamidur/ss/auth/service/MyAppUserDetails.java
@@ -0,0 +1,59 @@
+package com.hamidur.ss.auth.service;
+
+import com.hamidur.ss.auth.models.Role;
+import com.hamidur.ss.auth.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyAppUserDetails implements UserDetails
+{
+    private static final String ROLE_PREFIX = "ROLE_";
+    private final User user;
+
+    public MyAppUserDetails(final User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = user.getRoles();
+        Set<SimpleGrantedAuthority> grantedRoles = new HashSet<>();
+        roles.forEach( e -> grantedRoles.add(new SimpleGrantedAuthority(ROLE_PREFIX + e.getRole())));
+        return grantedRoles;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
