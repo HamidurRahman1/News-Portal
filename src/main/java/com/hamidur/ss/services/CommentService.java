e3323126adc6d1338afd20c441f8f950d02753d3
Hamidur Rahman <Hamidur0405@gmail.com>
2020-06-12 22:15:57 -0400

    adding more service method on commentservice

--- src/main/java/com/hamidur/ss/services/CommentService.java
+++ src/main/java/com/hamidur/ss/services/CommentService.java
@@ -22,9 +22,28 @@ public class CommentService
     {
         List<Comment> comments = commentRepository.findAll();
         if(comments == null)
-        {
             throw new NotFoundException("No comments found to return");
-        }
         return comments;
+    }
+
+    public Comment getCommentById(Integer commentId) throws NotFoundException
+    {
+        Comment comment = commentRepository.findByCommentId(commentId);
+        if(comment == null)
+            throw new NotFoundException("No comment found associated with commentId="+commentId);
+        return comment;
+    }
+
+    public List<Comment> getAllCommentsByArticleId(Integer articleId) throws NotFoundException
+    {
+        List<Comment> comments = commentRepository.getCommentsByArticleId(articleId);
+        if(comments == null)
+            throw new NotFoundException("No comments found associated with articleId="+articleId);
+        return comments;
+    }
+
+    public void updateCommentByCommentIdAndArticleId(Integer articleId, Integer commentId, String comment)
+    {
+        commentRepository.updateCommentByCommentIdAndArticleId(comment, commentId, articleId);
     }
 }
