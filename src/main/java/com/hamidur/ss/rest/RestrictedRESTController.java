d907e2f999cf77684c1e6ba1011c076ab5297441
Hamidur Rahman <Hamidur0405@gmail.com>
2020-07-06 13:17:52 -0400

    adding getArticlesBySubString()

--- src/main/java/com/hamidur/ss/rest/RestrictedRESTController.java
+++ src/main/java/com/hamidur/ss/rest/RestrictedRESTController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import javax.validation.constraints.PositiveOrZero;
@@ -54,6 +55,12 @@ public class RestrictedRESTController
     public ResponseEntity<Set<User>> getAuthors()
     {
         return new ResponseEntity<>(userService.getAllAuthors(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/articles/text")
+    public ResponseEntity<Set<Article>> searchArticle(@RequestParam("bodyContains") String subString)
+    {
+        return new ResponseEntity<>(articleService.getArticlesBySubString(subString), HttpStatus.OK);
     }
 
     @GetMapping(value = "/author/{authorId}", produces = MediaType.APPLICATION_JSON_VALUE)
